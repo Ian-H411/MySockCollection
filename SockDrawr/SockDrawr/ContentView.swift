a49699a0c7b180549f555dc104a14a861efb092b
Ian_H411 <ian.h411@gmail.com>
2019-11-22 14:49:40 -0700

    creates project

---
+++ SockDrawr/SockDrawr/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SockDrawr
+//
+//  Created by Ian Hall on 11/22/19.
+//  Copyright © 2019 Ian Hall. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
