7edcf564d6dd9f00ec2a917270b913aa07994f5e
Ian_H411 <ian.h411@gmail.com>
2019-12-08 20:15:48 -0700

    adds camera controlls

--- SocDrawr1.1/SocDrawr1.1/Views/SockCollectionViewController.swift
+++ SocDrawr1.1/SocDrawr1.1/Views/SockCollectionViewController.swift
@@ -96,4 +96,67 @@ class SockCollectionViewController: UICollectionViewController {
     }
     */
 
+    
+    //MARK: - Actions
+    
+    @IBAction func addSockButton(_ sender: Any) {
+        presentActionSheet()
+    }
+    
+    
+    
+    
+    //MARK: - Helpers
+    
+    func camera(){
+        if UIImagePickerController.isSourceTypeAvailable(.camera){
+            let myPickerController = UIImagePickerController()
+            myPickerController.delegate = self
+            myPickerController.sourceType = .camera
+            self.present(myPickerController, animated: true , completion: nil)
+        }
+    }
+    
+    func photoLibrary() {
+        if UIImagePickerController.isSourceTypeAvailable(.photoLibrary){
+            let mypickerController = UIImagePickerController()
+            mypickerController.delegate = self
+            mypickerController.sourceType = .photoLibrary
+            self.present(mypickerController, animated: true, completion: nil)
+        }
+    }
+    
+    func presentActionSheet(){
+        let actionSheet = UIAlertController(title: "Import Receipt Photo", message: nil, preferredStyle: .actionSheet)
+        if UIImagePickerController.isSourceTypeAvailable(.camera){
+            let cameraButton = UIAlertAction(title: "Import With Camera", style: .default) { (_) in
+                self.camera()
+            }
+            actionSheet.addAction(cameraButton)
+        }
+        if UIImagePickerController.isSourceTypeAvailable(.photoLibrary){
+            let photoLibrary = UIAlertAction(title: "Import From Photo Library", style: .default) { (_) in
+                self.photoLibrary()
+            }
+            actionSheet.addAction(photoLibrary)
+        }
+        let cancelButton = UIAlertAction(title: "Cancel", style: .destructive, handler: nil)
+        actionSheet.addAction(cancelButton)
+        self.present(actionSheet, animated: true, completion: nil)
+    }
+    
+}
+extension SockCollectionViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let image = info[.originalImage] as? UIImage{
+            
+        } else {
+            print("Something went wrong")
+        }
+        self.dismiss(animated: true, completion: nil)
+    }
 }
