3ada2a7a436de9b7248c6aaeb1a550f678c66ab7
Ian_H411 <ian.h411@gmail.com>
2020-02-05 14:09:02 -0600

    removes storyboard files and returns to original SwiftUI

---
+++ SocDrawr1.1/SocDrawr1.1/Resources/ArrayHelper.swift
@@ -0,0 +1,16 @@
+//
+//  ArrayHelper.swift
+//  SocDrawr1.1
+//
+//  Created by Ian Hall on 2/5/20.
+//  Copyright © 2020 Ian Hall. All rights reserved.
+//
+
+import Foundation
+
+extension Array {
+    
+    func chunked(by: Int) -> [[Element]] {
+        
+    }
+}
--- SocDrawr1.1/SocDrawr1.1/Views/AddSockViewController.swift
+++ SocDrawr1.1/SocDrawr1.1/Views/AddSockViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class AddSockViewController: UIViewController {
-//MARK: - Properties
+    //MARK: - Properties
     
     @IBOutlet weak var nameTextField: UITextField!
     
@@ -42,7 +42,7 @@ class AddSockViewController: UIViewController {
     
     //MARK: - Actions
     
-   
+    
     @IBAction func colorPickerButtonTapped(_ sender: Any) {
     }
     
@@ -80,15 +80,19 @@ class AddSockViewController: UIViewController {
         picker.delegate = self
         picker.dataSource = self
         let doneToolbar: UIToolbar = UIToolbar(frame: CGRect.init(x: 0, y: 0, width: UIScreen.main.bounds.width, height: 50))
-                 doneToolbar.barStyle = .default
-                 let flexSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
-                 let done: UIBarButtonItem = UIBarButtonItem(title: "Save", style: .done, target: self, action: #selector(self.doneButtonAction))
-                 let items = [flexSpace, done]
-                 doneToolbar.items = items
-                 doneToolbar.sizeToFit()
+        doneToolbar.barStyle = .default
+        let flexSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let done: UIBarButtonItem = UIBarButtonItem(title: "Save", style: .done, target: self, action: #selector(self.doneButtonAction))
+        let items = [flexSpace, done]
+        doneToolbar.items = items
+        doneToolbar.sizeToFit()
     }
-   
-
+    
+    func doneButtonAction() {
+        
+    }
+    
+    
 }
 //MARK: - Extensions
 
