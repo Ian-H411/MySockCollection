18acd02c28cdf816b2493921536c854782aa4f16
Ian_H411 <ian.h411@gmail.com>
2020-02-06 10:43:44 -0600

    fixes and adds navigation bar

--- MatchMySocks/MatchMySocks/Views/Main Views/ContentView.swift
+++ MatchMySocks/MatchMySocks/Views/Main Views/ContentView.swift
@@ -11,18 +11,31 @@ import SwiftUI
 struct ContentView: View {
     let socksChunked = SockController.shared.chunkedSocks
     var body: some View {
-        List {
-            ForEach(0..<socksChunked.count, id: \.self) { index in
-                HStack {
-                    ForEach(self.socksChunked[index], id: \.self) { sock in
-                        GridCell(sock: sock)
-                    }
+        NavigationView {
+            Form {
+                Section {
+                    Text("My Socks")
+                    .hidden()
                 }
             }
-            
+            .navigationBarTitle("My Socks")
+            .navigationBarItems(trailing: Button("Camera", action: {
+                print("Go to Camera")
+            }))
+            List {
+                ForEach(0..<socksChunked.count, id: \.self) { index in
+                    HStack {
+                        ForEach(self.socksChunked[index], id: \.self) { sock in
+                            GridCell(sock: sock)
+                        }
+                    }
+                }
+                
+            }
         }
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
