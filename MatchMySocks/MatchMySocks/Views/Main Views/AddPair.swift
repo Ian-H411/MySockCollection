2c99f0bfbd4a2618592e3493f19152c59efdab75
Ian_H411 <ian.h411@gmail.com>
2020-02-10 13:48:18 -0600

    adds fixes and acsess to button

--- MatchMySocks/MatchMySocks/Views/Main Views/AddPair.swift
+++ MatchMySocks/MatchMySocks/Views/Main Views/AddPair.swift
@@ -11,41 +11,42 @@ import UIKit.UIImage
 
 struct AddPair: View {
     let defaultImage = "empty"
-    @State var image: Image? = nil
+    @State var image: Image? = Image("empty")
     @State private var textField = ""
     @State var showCaptureImageView: Bool = false
     var body: some View {
         VStack {
             TextField("Name This Pair of Socks", text: $textField)
             Divider()
-            ZStack{
-                Button(action: {
-                    self.showCaptureImageView.toggle()
-                }) {
-                    Text("")
-                }
-                .clipShape(Circle())
-                Image(defaultImage).resizable()
+            
+            Button(action: {
+                self.showCaptureImageView.toggle()
+            }) {
+                image?
+                    .resizable()
                     .clipShape(Circle())
                     .overlay(Circle().stroke(Color.white, lineWidth: 4))
                     .shadow(radius: 0.5)
                     .frame(width: 350, height: 350, alignment: .center)
             }
+            
             if (showCaptureImageView) {
-              CaptureImageView(isShown: $showCaptureImageView, image: $image)
+                CaptureImageView(isShown: $showCaptureImageView, image: $image)
             }
             
         }.padding()
+        
+        
     }
-   
+    
 }
 
 struct CaptureImageView {
-  
+    
     @Binding var isShown: Bool
     @Binding var image: Image?
     func makeCoordinator() -> Coordinator {
-      return Coordinator(isShown: $isShown, image: $image)
+        return Coordinator(isShown: $isShown, image: $image)
     }
     
 }
@@ -57,31 +58,32 @@ extension CaptureImageView: UIViewControllerRepresentable {
     
     func makeUIViewController(context: UIViewControllerRepresentableContext<CaptureImageView>) -> UIImagePickerController {
         let picker = UIImagePickerController()
-               picker.delegate = context.coordinator
-               return picker
+        picker.delegate = context.coordinator
+        picker.sourceType = .camera
+        return picker
     }
     
     typealias UIViewControllerType = UIImagePickerController
     
 }
 class Coordinator: NSObject, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
-  @Binding var isCoordinatorShown: Bool
-  @Binding var imageInCoordinator: Image?
-  init(isShown: Binding<Bool>, image: Binding<Image?>) {
-    _isCoordinatorShown = isShown
-    _imageInCoordinator = image
-  }
+    @Binding var isCoordinatorShown: Bool
+    @Binding var imageInCoordinator: Image?
+    init(isShown: Binding<Bool>, image: Binding<Image?>) {
+        _isCoordinatorShown = isShown
+        _imageInCoordinator = image
+    }
     
-  func imagePickerController(_ picker: UIImagePickerController,
-                didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-     guard let unwrapImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage else { return }
-     imageInCoordinator = Image(uiImage: unwrapImage)
-     isCoordinatorShown = false
-  }
+    func imagePickerController(_ picker: UIImagePickerController,
+                               didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let unwrapImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage else { return }
+        imageInCoordinator = Image(uiImage: unwrapImage)
+        isCoordinatorShown = false
+    }
     
-  func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
-     isCoordinatorShown = false
-  }
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        isCoordinatorShown = false
+    }
 }
 
 struct AddPair_Previews: PreviewProvider {
